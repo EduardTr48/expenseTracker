10e1b8f9005b7fdaee079937e195d7aa58929d99
Eduardo Rodriguez <tiago.edu.rodriguez@gmail.com>
2024-09-15 01:39:19 -0300

    update UI in ReportMonth

--- src/components/ReportMonth.jsx
+++ src/components/ReportMonth.jsx
@@ -1,23 +1,23 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from 'recharts';
-import { useExpenses } from '../context/ExpensesContext';
-import { obtenerGastosPorFecha } from '../helpers/obtenerGastosPorFecha';
-import { obtenerGastosPorCategoria } from '../helpers/obtenerGastosPorCategoria';
-import { useState } from 'react';
-import { formatDDYY } from '../helpers/formatDate';
-import { obtenerGastoTotal } from '../helpers/obtenerGastoTotal';
-import { useCategories } from '../context/CategoriesContext';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from "recharts";
+import { useExpenses } from "../context/ExpensesContext";
+import { obtenerGastosPorFecha } from "../helpers/obtenerGastosPorFecha";
+import { obtenerGastosPorCategoria } from "../helpers/obtenerGastosPorCategoria";
+import { useState } from "react";
+import { formatDDYY } from "../helpers/formatDate";
+import { obtenerGastoTotal } from "../helpers/obtenerGastoTotal";
+import { useCategories } from "../context/CategoriesContext";
 const ReportMonth = () => {
     const { expenses, error } = useExpenses();
     const { categoriesMap } = useCategories();
-    const [date, setDate] = useState('08-2024');
+    const [date, setDate] = useState("08-2024");
     const dataFilterDate = obtenerGastosPorFecha(expenses, date);
     const data = obtenerGastosPorCategoria(dataFilterDate, categoriesMap);
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A4DE02', '#D0ED57', '#FF7F50'];
+    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#A4DE02", "#D0ED57", "#FF7F50"];
     const gastoTotal = obtenerGastoTotal(dataFilterDate);
     const CustomTooltip = ({ active, payload }) => {
         if (active && payload && payload.length) {
             return (
-                <div style={{ backgroundColor: '#333', color: '#fff', padding: '10px', borderRadius: '5px' }}>
+                <div style={{ backgroundColor: "#333", color: "#fff", padding: "10px", borderRadius: "5px" }}>
                     <p>{`${payload[0].name} : $${payload[0].value}`}</p>
                 </div>
             );
@@ -25,7 +25,7 @@ const ReportMonth = () => {
     };
 
     const convertStringToDate = (mmYYYY) => {
-        const [month, year] = mmYYYY.split('-').map(Number);
+        const [month, year] = mmYYYY.split("-").map(Number);
         return new Date(year, month - 1);
     };
 
@@ -41,11 +41,15 @@ const ReportMonth = () => {
 
     return (
         <>
-            <p>Gasto Total: ${gastoTotal}</p>
-            <div className="flex justify-between">
-                <button onClick={() => changeMonth(-1)}>Anterior</button>
-                <h2 className="text-center">{date}</h2>
-                <button onClick={() => changeMonth(1)}>Siguiente</button>
+            <p className="text-2xl">Gasto Total: ${gastoTotal}</p>
+            <div className="my-5 flex justify-between">
+                <button className="px-4 py-2 bg-slate-900" onClick={() => changeMonth(-1)}>
+                    Anterior
+                </button>
+                <h2 className="text-center text-2xl">{date}</h2>
+                <button className="px-4 py-2 bg-slate-900" onClick={() => changeMonth(1)}>
+                    Siguiente
+                </button>
             </div>
             {gastoTotal ? (
                 <ResponsiveContainer width="100%" height={400}>
